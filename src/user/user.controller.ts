a70d73299ea62809bea2730653ad25134a3a3d90
weisuoke <530264127@qq.com>
2023-04-11 00:36:21 +0800

    feat: 🎸 update user module. use pg & typeorm

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,18 +1,47 @@
 import {
   Body,
   Controller,
+  Delete,
+  Get,
+  Param,
   Post,
+  Put,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 import { CreateUserDto } from './create-user.dto';
+import { UserService } from './user.service';
 
 @Controller('users')
 export class UserController {
+  constructor(private readonly userService: UserService) {}
+
   @Post()
   @UsePipes(new ValidationPipe())
-  createUser(@Body() createUserDto: CreateUserDto): string {
-    // 在这里处理创建用户的逻辑
-    return `Creating a user with name: ${createUserDto.name} and email: ${createUserDto.email}`;
+  async createUser(@Body() createUserDto: CreateUserDto) {
+    return await this.userService.createUser(createUserDto);
+  }
+
+  @Get()
+  async findAllUsers() {
+    return await this.userService.findAllUsers();
+  }
+
+  @Get(':id')
+  async findUserById(@Param('id') id: number) {
+    return await this.userService.findUserById(id);
+  }
+
+  @Put(':id')
+  async updateUser(
+    @Param('id') id: number,
+    @Body() updateUserDto: Partial<CreateUserDto>,
+  ) {
+    return await this.userService.updateUser(id, updateUserDto);
+  }
+
+  @Delete(':id')
+  async deleteUser(@Param('id') id: number) {
+    return await this.userService.deleteUser(id);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,10 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateUserDto } from './create-user.dto';
+import { User } from './user.entity';
 
 @Injectable()
 export class UserService {
-  createUser(createUserDto: CreateUserDto): string {
-    // 在这里处理创建用户的逻辑
-    return `Creating a user with name: ${createUserDto.name} and email: ${createUserDto.email}`;
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async createUser(createUserDto: CreateUserDto): Promise<User> {
+    const newUser = this.userRepository.create(createUserDto);
+    return await this.userRepository.save(newUser);
+  }
+
+  async findAllUsers(): Promise<User[]> {
+    return await this.userRepository.find();
+  }
+
+  async findUserById(id: number): Promise<User | undefined> {
+    return await this.userRepository.findOne({ where: { id } });
+  }
+
+  async updateUser(
+    id: number,
+    updateUserDto: Partial<CreateUserDto>,
+  ): Promise<User | undefined> {
+    const user = await this.userRepository.findOne({ where: { id } });
+    if (!user) {
+      return undefined;
+    }
+    await this.userRepository.update(id, updateUserDto);
+    return await this.userRepository.findOne({ where: { id } });
+  }
+
+  async deleteUser(id: number): Promise<boolean> {
+    const result = await this.userRepository.delete(id);
+    return result.affected > 0;
   }
 }
